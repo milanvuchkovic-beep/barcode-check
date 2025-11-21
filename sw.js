23e21dc6988d19c108c75df211c37e2aad995121
milanvuchkovic-beep <milanvuchkovic@gmail.com>
2025-11-21 13:39:49 +0100

    Update service worker to version 2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'skener-app-v1';
+const CACHE_NAME = 'skener-app-v2';
 const FILES_TO_CACHE = [
     './',
     './index.html',
@@ -42,3 +42,4 @@ self.addEventListener('fetch', (event) => {
         })
     );
 });
+
